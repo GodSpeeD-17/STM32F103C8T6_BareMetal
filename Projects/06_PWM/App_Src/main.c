5efa389129ea83e4c692ee1f0ecfc6d19bd8c1b9
GodSpeeD <shahshrey1110+GitHub@gmail.com>
2024-11-23 20:45:32 +0530

    Updated 06_PWM

--- Projects/06_PWM/App_Src/main.c
+++ Projects/06_PWM/App_Src/main.c
@@ -45,7 +45,7 @@ int main(void){
 	enable_GPT(&pwm_config.GPT_CONFIGx);
 
 	// Local Variable
-	uint8_t dutyCycle = 0x00;
+	uint8_t dutyCycle;
 	
 	while(1){
 
@@ -83,76 +83,3 @@ int main(void){
 	// Return Value
 	return 0;
 }
-
-/*
-// Old Code
-// Main Entry Point for User Code
-int main(void){
-	
-	// Configure Clock
-	config_SYSCLK_MHz(SYSCLK_MHz);
-
-	// SysTick Timer (1ms)
-	config_SysTick(CoreClock/1000);
-
-	// Configure GPIO as AF
-	config_GPIO(LED_PORT, LED_PIN, LED_MODE, CNF_OUT_AF_PP);
-
-	// Configure On-board LED
-	config_OB_LED();
-	reset_OB_LED();
-
-	// Enable Clock for GP Timer
-	enable_GPT_clk(GP_TIMER);
-
-	// Configure GP Timer (Frequency)
-	config_GPT(GP_TIMER, GPT_ARR, GPT_FREQ, GPT_CNT);
-
-	// Configure PWM Mode (Normal)
-	config_PWM_mode(GP_TIMER, GPT_CHANNEL, TIMx_OCM_PWM_NORMAL, PRELOAD_ENABLE);
-	
-	// Channel Enable
-	enable_GPT_CH(GP_TIMER, GPT_CHANNEL);
-
-	// Timer Enable
-	enable_GPT(GP_TIMER);
-
-	// Local Variable
-	uint8_t dutyCycle = 0xFF;
-	
-	while(1){
-
-		// Increase Brightness
-		for(dutyCycle = MIN_DUTY_CYCLE; dutyCycle <= MAX_DUTY_CYCLE; dutyCycle += 2){
-			// Set Duty Cycle
-			set_PWM_duty_cycle(GP_TIMER, GPT_CHANNEL, dutyCycle);
-			// Delay
-			SysTick_delay_ms(DELAY_MS);
-		}
-		
-		// Indication
-		toggle_OB_LED();
-		SysTick_delay_ms(2000);
-		toggle_OB_LED();
-
-		// Decrease Brightness
-		for(dutyCycle = MAX_DUTY_CYCLE; dutyCycle >= MIN_DUTY_CYCLE; dutyCycle -= 2){
-			// Set Duty Cycle
-			set_PWM_duty_cycle(GP_TIMER, GPT_CHANNEL, dutyCycle);
-			// Delay
-			SysTick_delay_ms(DELAY_MS);
-		}
-		
-		// Reset Pin 
-		// reset_GPIO(LED_PORT, LED_PIN);
-
-		// Indication
-		toggle_OB_LED();
-		SysTick_delay_ms(2000);
-		toggle_OB_LED();
-	}
-
-	// Return Value
-	return 0;
-}
-*/
