ebe3071e819c0a53d8b3b4ef1a8ba541537314e8
GodSpeeD <shahshrey1110+GitHub@gmail.com>
2024-12-01 11:00:53 +0530

    Updated 08_ADC

--- BareMetal/Src/adc.c
+++ BareMetal/Src/adc.c
@@ -15,14 +15,23 @@
 void config_ADC(ADC_REG_STRUCT* ADCx, uint8_t channel, uint8_t cc){
     // Enable Clock for ADC
     enable_ADC_clk(ADCx);
-    // Clear the Control Register
+    // Clear the Control Register, Sample Time Register, Regular Sequence Register
     ADCx->CR2.REG = (uint32_t) 0x00000000;
-    // Setup Channel
-    ADCx->SQR3.REG = channel;
-	// Wait for for time (tSTAB)
-	for(volatile uint16_t demo = 0; demo <= ADC_ON_DELAY; demo++);
+    ADCx->SMPR2.REG = (uint32_t) 0x000000;
+    ADCx->SQR1.REG = (uint32_t) 0x000000;
+    ADCx->SQR3.REG = (uint32_t) 0x000000;
+    // Set Maximum Sampling time for ADC Channel
+    ADCx->SMPR2.REG |= (0x07 << (channel * 3));
+    // Set number of Channel Conversion (0x00 = 1 conversion)
+    ADCx->SQR1.REG |= (0x00 << 20);
+    // Setup Channel Sequence for Conversion
+    ADCx->SQR3.REG |= channel;
     // Enable/Disable Continuous Conversion
     ADCx->CR2.BIT.CONT = cc;
+    // When the ADON bit is set for the first time, it wakes up the ADC from Power Down mode
+	enable_ADC(ADCx);
+	// Wait for for time (tSTAB)
+	for(volatile uint16_t demo = 0; demo <= ADC_ON_DELAY; demo++);
     // When the ADON bit is set for the first time, it wakes up the ADC from Power Down mode
 	enable_ADC(ADCx);
     // Calibration of ADC
@@ -38,14 +47,15 @@ void config_ADC(ADC_REG_STRUCT* ADCx, uint8_t channel, uint8_t cc){
 uint16_t get_ADC_raw_data(ADC_REG_STRUCT* ADCx){
 	// Final Result
 	uint16_t result;
-	// Conversion starts when ADON bit is set for a second time by software after ADC power-up time (tSTAB)
-	enable_ADC(ADCx);
+    // Start the conversion using Software as Trigger
+    ADCx->CR2.REG |= (1 << 22);
     // Wait for End of Conversion
     while(!ADCx->SR.BIT.EOC);
     // Read the ADC Data (Generally, It is cleared by software or by reading the ADC_DR)
 	result = (ADCx->DR.REG & 0x0FFF);
     // Just for backup
+    ADCx->CR2.REG &= ~(1 << 22);
     ADCx->SR.BIT.EOC = BIT_RESET;
     // 12-bit ADC Data
-    return result;
+    return (result & 0x0FFF);
 }
