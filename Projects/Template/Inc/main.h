5954bf28d25d140aeff693e8e490fc040634345a
GodSpeeD <shahshrey1110+GitHub@gmail.com>
2025-01-25 19:40:37 +0530

    Dependency Issue

---
+++ Projects/Template/Inc/main.h
@@ -0,0 +1,11 @@
+// Header Guards
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+// #include "bare_metal.h"
+#include "common.h"
+#include "reg_map.h"
+#include "systick.h"
+#include "gpio.h"
+
+#endif /* __MAIN_H__ */ 
--- Projects/Template/Src/main.c
+++ Projects/Template/Src/main.c
@@ -1,20 +1,43 @@
-#include "systick.h"
+#include "main.h"
 
 int main(void){
-	config_SysTick(SystemCoreClock/1000);
-	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN | RCC_APB2ENR_IOPAEN;
-	GPIOC->CRH &= ~(GPIO_CRH_CNF13_0);
-	GPIOC->CRH |= (GPIO_CRH_MODE13_0); 
-	GPIOA->CRL &= ~(GPIO_CRL_CNF2_0);
-	GPIOA->CRL |= (GPIO_CRL_MODE2_0); 
-	GPIOC->ODR |= GPIO_ODR_ODR13;
-	delay_ms(1000);
-	GPIOC->ODR &= ~GPIO_ODR_ODR13;
-	delay_ms(1000);
-	GPIOA->ODR |= GPIO_ODR_ODR2;
 	
+	// PLL
+	uint32_t reg = 0x00;
+	// Configure Flash for Two Wait States
+	FLASH->ACR.REG = FLASH_ACR_LATENCY_2 | FLASH_ACR_PRFTBE;
+	while(!(FLASH->ACR.REG & FLASH_ACR_PRFTBS_Msk));
+	RCC->CR.REG |= RCC_CR_HSEON;
+	while(!(RCC->CR.REG & RCC_CR_HSERDY_Msk));
+	// PLL Configuration
+	reg |= RCC_CFGR_PLLMULL9 | RCC_CFGR_PLLXTPRE_HSE | RCC_CFGR_PLLSRC;
+	// Bus Prescaler Configuration
+	reg |= RCC_CFGR_PPRE2_DIV1 | RCC_CFGR_PPRE1_DIV2 | RCC_CFGR_HPRE_DIV1;
+	// USB_DIV_1.5: 48MHz
+	reg &= ~RCC_CFGR_USBPRE_Msk;
+	// ADC_DIV_6
+	reg |= RCC_CFGR_ADCPRE_DIV6;
+	RCC->CFGR.REG = reg;
+	// PLL ON
+	RCC->CR.REG |= RCC_CR_PLLON;
+	while(!(RCC->CR.REG & RCC_CR_PLLRDY));
+	// System Clock Switch
+	RCC->CFGR.REG |= RCC_CFGR_SW_PLL;
+	while((RCC->CFGR.REG & RCC_CFGR_SWS_Msk) != RCC_CFGR_SWS_PLL);
+
+	// 1ms
+	config_SysTick(72*1000);
+	
+	// OB LED
+	config_OB_LED();
+	reset_OB_LED();
+
+
+	// Infinite Loop
 	while(1){
-		GPIOC->ODR ^= GPIO_ODR_ODR13;
+		// 
+		toggle_OB_LED();
+		// 1s
 		delay_ms(1000);
 	}
 	
