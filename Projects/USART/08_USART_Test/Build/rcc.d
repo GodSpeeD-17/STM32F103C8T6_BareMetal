fb417b3e1e7eacff6858aaa99511f44c53b04826
GodSpeeD <shahshrey1110+GitHub@gmail.com>
2025-02-08 13:02:19 +0530

    Temp 08->Proper Debug

---
+++ Projects/USART/08_USART_Test/Build/rcc.d
@@ -0,0 +1,34 @@
+/home/shrey_shah/Desktop/STM32/STM32F103C8T6/Projects/USART/08_USART_Test/Build/rcc.o: \
+ /home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Driver/Src/rcc.c \
+ /home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Driver/Inc/rcc.h \
+ /home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Driver/Inc/reg_map.h \
+ /home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/flash_reg_map.h \
+ /home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/common.h \
+ /home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/cmsis_gcc.h \
+ /home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/nvic_reg_map.h \
+ /home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/systick_reg_map.h \
+ /home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/wwdg_reg_map.h \
+ /home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/adc_reg_map.h \
+ /home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/advtim_reg_map.h \
+ /home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/afio_reg_map.h \
+ /home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/exti_reg_map.h \
+ /home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/gpio_reg_map.h \
+ /home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/gpt_reg_map.h \
+ /home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/rcc_reg_map.h \
+ /home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/usart_reg_map.h
+/home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Driver/Inc/rcc.h:
+/home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Driver/Inc/reg_map.h:
+/home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/flash_reg_map.h:
+/home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/common.h:
+/home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/cmsis_gcc.h:
+/home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/nvic_reg_map.h:
+/home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/systick_reg_map.h:
+/home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/wwdg_reg_map.h:
+/home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/adc_reg_map.h:
+/home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/advtim_reg_map.h:
+/home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/afio_reg_map.h:
+/home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/exti_reg_map.h:
+/home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/gpio_reg_map.h:
+/home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/gpt_reg_map.h:
+/home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/rcc_reg_map.h:
+/home/shrey_shah/Desktop/STM32/STM32F103C8T6/BareMetal/Core/Inc/usart_reg_map.h:
