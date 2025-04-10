05a3d79da74fadad66d66dbd2150efc798539cd6
GodSpeeD <shahshrey1110+GitHub@gmail.com>
2025-04-10 21:03:13 +0530

    Adding DMA

--- BareMetal/Core/Inc/dma_reg_map.h
+++ BareMetal/Core/Inc/dma_reg_map.h
@@ -2,8 +2,10 @@
 #ifndef __DMA_REG_MAP_H__
 #define __DMA_REG_MAP_H__
 
+// Dependency
 #include "common.h"
 
+// DMA Register Structure
 typedef struct {
 	// DMA Interrupt Status Register (DMA_ISR)
 	union {
@@ -49,8 +51,8 @@ typedef struct {
 			// Reserved
 			volatile uint32_t RESERVED: 4;
 		} BIT;
-	}ISR;
-	// DMA Interrupt Flag Clear Register (DMA_IFR)
+	} ISR;
+	// DMA Interrupt Flag Clear Register (DMA_IFCR)
 	union {
 		// Full 32-bit Register Access
 		uint32_t REG;
@@ -94,408 +96,66 @@ typedef struct {
 			// Reserved
 			volatile uint32_t RESERVED: 4;
 		} BIT;
-	}IFR;
-	// DMA Channel 1 Configuration Register (DMA_CCR1)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			volatile uint32_t EN: 1;
-			volatile uint32_t TCIE: 1;
-			volatile uint32_t HTIE: 1;
-			volatile uint32_t TEIE: 1;
-			volatile uint32_t DIR: 1;
-			volatile uint32_t CIRC: 1;
-			volatile uint32_t PINC: 1;
-			volatile uint32_t MINC: 1;
-			volatile uint32_t PSIZE: 2;
-			volatile uint32_t MSIZE: 2;
-			volatile uint32_t PL: 2;
-			volatile uint32_t MEM2MEM: 1;
-			// Reserved
-			volatile uint32_t RESERVED: 17;
-		} BIT;
-	}CCR1;
-	// DMA Channel 2 Configuration Register (DMA_CCR2)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			volatile uint32_t EN: 1;
-			volatile uint32_t TCIE: 1;
-			volatile uint32_t HTIE: 1;
-			volatile uint32_t TEIE: 1;
-			volatile uint32_t DIR: 1;
-			volatile uint32_t CIRC: 1;
-			volatile uint32_t PINC: 1;
-			volatile uint32_t MINC: 1;
-			volatile uint32_t PSIZE: 2;
-			volatile uint32_t MSIZE: 2;
-			volatile uint32_t PL: 2;
-			volatile uint32_t MEM2MEM: 1;
-			// Reserved
-			volatile uint32_t RESERVED: 17;
-		} BIT;
-	}CCR2;
-	// DMA Channel 3 Configuration Register (DMA_CCR3)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			volatile uint32_t EN: 1;
-			volatile uint32_t TCIE: 1;
-			volatile uint32_t HTIE: 1;
-			volatile uint32_t TEIE: 1;
-			volatile uint32_t DIR: 1;
-			volatile uint32_t CIRC: 1;
-			volatile uint32_t PINC: 1;
-			volatile uint32_t MINC: 1;
-			volatile uint32_t PSIZE: 2;
-			volatile uint32_t MSIZE: 2;
-			volatile uint32_t PL: 2;
-			volatile uint32_t MEM2MEM: 1;
-			// Reserved
-			volatile uint32_t RESERVED: 17;
-		} BIT;
-	}CCR3;
-	// DMA Channel 4 Configuration Register (DMA_CCR4)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			volatile uint32_t EN: 1;
-			volatile uint32_t TCIE: 1;
-			volatile uint32_t HTIE: 1;
-			volatile uint32_t TEIE: 1;
-			volatile uint32_t DIR: 1;
-			volatile uint32_t CIRC: 1;
-			volatile uint32_t PINC: 1;
-			volatile uint32_t MINC: 1;
-			volatile uint32_t PSIZE: 2;
-			volatile uint32_t MSIZE: 2;
-			volatile uint32_t PL: 2;
-			volatile uint32_t MEM2MEM: 1;
-			// Reserved
-			volatile uint32_t RESERVED: 17;
-		} BIT;
-	}CCR4;
-	// DMA Channel 5 Configuration Register (DMA_CCR5)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			volatile uint32_t EN: 1;
-			volatile uint32_t TCIE: 1;
-			volatile uint32_t HTIE: 1;
-			volatile uint32_t TEIE: 1;
-			volatile uint32_t DIR: 1;
-			volatile uint32_t CIRC: 1;
-			volatile uint32_t PINC: 1;
-			volatile uint32_t MINC: 1;
-			volatile uint32_t PSIZE: 2;
-			volatile uint32_t MSIZE: 2;
-			volatile uint32_t PL: 2;
-			volatile uint32_t MEM2MEM: 1;
-			// Reserved
-			volatile uint32_t RESERVED: 17;
-		} BIT;
-	}CCR5;
-	// DMA Channel 6 Configuration Register (DMA_CCR6)
-	union {
-		// Full 32-bit Register Access// DMA Channel X Configuration Register (DMA_CCRx)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			volatile uint32_t EN: 1;
-			volatile uint32_t TCIE: 1;
-			volatile uint32_t HTIE: 1;
-			volatile uint32_t TEIE: 1;
-			volatile uint32_t DIR: 1;
-			volatile uint32_t CIRC: 1;
-			volatile uint32_t PINC: 1;
-			volatile uint32_t MINC: 1;
-			volatile uint32_t PSIZE: 2;
-			volatile uint32_t MSIZE: 2;
-			volatile uint32_t PL: 2;
-			volatile uint32_t MEM2MEM: 1;
-			// Reserved
-			volatile uint32_t RESERVED: 17;
-		} BIT;
-	}CCR1;
-		uint32_t REG;
-		// Bit Access
-		struct {
-			volatile uint32_t EN: 1;
-			volatile uint32_t TCIE: 1;
-			volatile uint32_t HTIE: 1;
-			volatile uint32_t TEIE: 1;
-			volatile uint32_t DIR: 1;
-			volatile uint32_t CIRC: 1;
-			volatile uint32_t PINC: 1;
-			volatile uint32_t MINC: 1;
-			volatile uint32_t PSIZE: 2;
-			volatile uint32_t MSIZE: 2;
-			volatile uint32_t PL: 2;
-			volatile uint32_t MEM2MEM: 1;
-			// Reserved
-			volatile uint32_t RESERVED: 17;
-		} BIT;
-	}CCR6;
-	// DMA Channel 7 Configuration Register (DMA_CCR7)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			volatile uint32_t EN: 1;
-			volatile uint32_t TCIE: 1;
-			volatile uint32_t HTIE: 1;
-			volatile uint32_t TEIE: 1;
-			volatile uint32_t DIR: 1;
-			volatile uint32_t CIRC: 1;
-			volatile uint32_t PINC: 1;
-			volatile uint32_t MINC: 1;
-			volatile uint32_t PSIZE: 2;
-			volatile uint32_t MSIZE: 2;
-			volatile uint32_t PL: 2;
-			volatile uint32_t MEM2MEM: 1;
-			// Reserved
-			volatile uint32_t RESERVED: 17;
-		} BIT;
-	}CCR7;
-	// DMA Channel 1 Number of Data Register (DMA_CNDTR1)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			// Number of data to transfer
-			volatile uint32_t NDT: 1;
-			// Reserved
-			volatile uint32_t RESERVED: 17;
-		} BIT;
-	}CNDTR1;
-	// DMA Channel 2 Number of Data Register (DMA_CNDTR2)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			// Number of data to transfer
-			volatile uint32_t NDT: 1;
-			// Reserved
-			volatile uint32_t RESERVED: 17;
-		} BIT;
-	}CNDTR2;
-	// DMA Channel 3 Number of Data Register (DMA_CNDTR3)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			// Number of data to transfer
-			volatile uint32_t NDT: 1;
-			// Reserved
-			volatile uint32_t RESERVED: 17;
-		} BIT;
-	}CNDTR3;
-	// DMA Channel 4 Number of Data Register (DMA_CNDTR4)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			// Number of data to transfer
-			volatile uint32_t NDT: 1;
-			// Reserved
-			volatile uint32_t RESERVED: 17;
-		} BIT;
-	}CNDTR4;
-	// DMA Channel 5 Number of Data Register (DMA_CNDTR5)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			// Number of data to transfer
-			volatile uint32_t NDT: 1;
-			// Reserved
-			volatile uint32_t RESERVED: 17;
-		} BIT;
-	}CNDTR5;
-	// DMA Channel 6 Number of Data Register (DMA_CNDTR6)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			// Number of data to transfer
-			volatile uint32_t NDT: 1;
-			// Reserved
-			volatile uint32_t RESERVED: 17;
-		} BIT;
-	}CNDTR6;
-	// DMA Channel 7 Number of Data Register (DMA_CNDTR7)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			// Number of data to transfer
-			volatile uint32_t NDT: 1;
-			// Reserved
-			volatile uint32_t RESERVED: 17;
-		} BIT;
-	}CNDTR7;
-	// DMA Channel 1 Peripheral Address Register (DMA_CPAR1)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			// Peripheral Address
-			volatile uint32_t PA;
-		} BIT;
-	}CPAR1;
-	// DMA Channel 2 Peripheral Address Register (DMA_CPAR2)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			// Peripheral Address
-			volatile uint32_t PA;
-		} BIT;
-	}CPAR2;
-	// DMA Channel 3 Peripheral Address Register (DMA_CPAR3)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			// Peripheral Address
-			volatile uint32_t PA;
-		} BIT;
-	}CPAR3;
-	// DMA Channel 4 Peripheral Address Register (DMA_CPAR4)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			// Peripheral Address
-			volatile uint32_t PA;
-		} BIT;
-	}CPAR4;
-	// DMA Channel 5 Peripheral Address Register (DMA_CPAR5)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			// Peripheral Address
-			volatile uint32_t PA;
-		} BIT;
-	}CPAR5;
-	// DMA Channel 6 Peripheral Address Register (DMA_CPAR6)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			// Peripheral Address
-			volatile uint32_t PA;
-		} BIT;
-	}CPAR6;
-	// DMA Channel 7 Peripheral Address Register (DMA_CPAR7)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			// Peripheral Address
-			volatile uint32_t PA;
-		} BIT;
-	}CPAR7;
-	// DMA Channel 1 Memory Address Register (DMA_CMAR1)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			// Memory Address
-			volatile uint32_t MA;
-		} BIT;
-	}CMAR1;
-	// DMA Channel 2 Memory Address Register (DMA_CMAR2)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			// Memory Address
-			volatile uint32_t MA;
-		} BIT;
-	}CMAR2;
-	// DMA Channel 3 Memory Address Register (DMA_CMAR3)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			// Memory Address
-			volatile uint32_t MA;
-		} BIT;
-	}CMAR3;
-	// DMA Channel 4 Memory Address Register (DMA_CMAR4)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			// Memory Address
-			volatile uint32_t MA;
-		} BIT;
-	}CMAR4;
-	// DMA Channel 5 Memory Address Register (DMA_CMAR5)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			// Memory Address
-			volatile uint32_t MA;
-		} BIT;
-	}CMAR5;
-	// DMA Channel 6 Memory Address Register (DMA_CMAR6)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			// Memory Address
-			volatile uint32_t MA;
-		} BIT;
-	}CMAR6;
-	// DMA Channel 7 Memory Address Register (DMA_CMAR7)
-	union {
-		// Full 32-bit Register Access
-		uint32_t REG;
-		// Bit Access
-		struct {
-			// Memory Address
-			volatile uint32_t MA;
-		} BIT;
-	}CMAR7;
-
+	} IFCR;
 } DMA_REG_STRUCT;
+
+// DMA Channel Register Structure Configuration
+typedef struct {
+	// DMA Channel 1 Configuration Register (DMA_CCR)
+	union {
+		// Full 32-bit Register Access
+		uint32_t REG;
+		// Bit Access
+		struct {
+			volatile uint32_t EN: 1;
+			volatile uint32_t TCIE: 1;
+			volatile uint32_t HTIE: 1;
+			volatile uint32_t TEIE: 1;
+			volatile uint32_t DIR: 1;
+			volatile uint32_t CIRC: 1;
+			volatile uint32_t PINC: 1;
+			volatile uint32_t MINC: 1;
+			volatile uint32_t PSIZE: 2;
+			volatile uint32_t MSIZE: 2;
+			volatile uint32_t PL: 2;
+			volatile uint32_t MEM2MEM: 1;
+			// Reserved
+			volatile uint32_t RESERVED: 17;
+		} BIT;
+	} CCR;
+	// DMA Channel  Number of Data Register (DMA_CNDTR)
+	union {
+		// Full 32-bit Register Access
+		uint32_t REG;
+		// Bit Access
+		struct {
+			// Number of data to transfer
+			volatile uint32_t NDT: 1;
+			// Reserved
+			volatile uint32_t RESERVED: 17;
+		} BIT;
+	} CNDTR;
+	// DMA Channel  Peripheral Address Register (DMA_CPAR)
+	union {
+		// Full 32-bit Register Access
+		uint32_t REG;
+		// Bit Access
+		struct {
+			// Peripheral Address
+			volatile uint32_t PA;
+		} BIT;
+	} CPAR;
+	// DMA Channel  Memory Address Register (DMA_CMAR)
+	union {
+		// Full 32-bit Register Access
+		uint32_t REG;
+		// Bit Access
+		struct {
+			// Memory Address
+			volatile uint32_t MA;
+		} BIT;
+	} CMAR;
+} DMA_CHANNEL_REG_STRUCT;
 
 
 #endif /* __DMA_REG_MAP_H__ */ 
