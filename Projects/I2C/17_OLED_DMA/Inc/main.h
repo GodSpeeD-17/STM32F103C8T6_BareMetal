9da1e18586071d1b61cebbf0720a25f9089bbdb6
GodSpeeD <shahshrey1110+GitHub@gmail.com>
2025-04-20 11:08:08 +0530

    I2C DMA Not working

--- Projects/I2C/17_OLED_DMA/Inc/main.h
+++ Projects/I2C/17_OLED_DMA/Inc/main.h
@@ -5,39 +5,23 @@
 /*-------------------------------------------------------------------------------*/
 // Headers
 #include "bare_metal.h"
+#include "ssd1306.h"
 /*-------------------------------------------------------------------------------*/
 
 /*-------------------------------------------------------------------------------*/
 // MACROS
 #define LOOP_DELAY_MS						((uint32_t) 1000)
-#define BUFFER_SIZE							((uint8_t) 128)
+#define BUFFER_SIZE							128
 /*-------------------------------------------------------------------------------*/
 // Variables
-__attribute__ ((aligned(4))) uint8_t src_buffer[BUFFER_SIZE] = {[0 ... 127] = 0xAA}; 
-__attribute__ ((aligned(4))) uint8_t dst_buffer[BUFFER_SIZE];
+uint8_t src_buffer[BUFFER_SIZE + 1] = {[0 ... BUFFER_SIZE] = 0xFF};
 
+// DMA Configuration
+dma_config_t DMA_SSD1306_Configuration;
+// I2C Configuration Structure
+i2c_config_t I2C_SSD1306_Configuration;
 
-// Example configuration for memory copy
-dma_channel_config_t mem2mem_config = {
-    .direction = DMAx_DIR_READ_FROM_PER, // Direction doesn't matter for MEM2MEM
-    .mem2mem = DMAx_MEM2MEM_ENABLE,      // Enable memory-to-memory mode
-    .circular_mode = DMAx_CIRC_DISABLE,
-    .priority = DMAx_PRIORITY_HIGH
-};
-
-dma_channel_data_t mem2mem_data_config = {
-    .srcDataSize = DMAx_DATA_SIZE_BIT_8,  // 8-bit transfers
-    .dstDataSize = DMAx_DATA_SIZE_BIT_8,  // Must match source size
-    .srcInc = DMAx_INC_ENABLE,
-    .dstInc = DMAx_INC_ENABLE
-};
-
-dma_channel_intr_t mem2mem_irq_config = {
-    .TCIE = DMAx_IRQ_ENABLE,  // Transfer complete interrupt
-    .HTIE = DMAx_IRQ_DISABLE,
-    .TEIE = DMAx_IRQ_ENABLE   // Error interrupt
-};
-
+// LED Configuration
 gpio_config_t LED_Configuration = {
 	.GPIO = GPIOA,
 	.PIN = GPIOx_PIN_2,
@@ -45,5 +29,6 @@ gpio_config_t LED_Configuration = {
 	.CNF = GPIOx_CNF_OUT_GP_PP
 };
 /*-------------------------------------------------------------------------------*/
+void I2C1_EV_IRQHandler(void);
 
 #endif /* __MAIN_H__ */ 
--- Projects/I2C/17_OLED_DMA/Src/main.c
+++ Projects/I2C/17_OLED_DMA/Src/main.c
@@ -2,40 +2,202 @@
 // Header Files
 #include "main.h"
 /*-------------------------------------------------------------------------------*/
-
-
+// I2C Status
+static volatile uint8_t i2c_status = 0x00;
 /*-------------------------------------------------------------------------------*/
 // Main Entry Point
 int main(){
-	
 
 	// Configure GPIO
 	GPIO_Config(&LED_Configuration);
 	// Reset GPIO
 	GPIO_Reset(&LED_Configuration);
 
-	// Enable DMA Transfer
-	DMA_CH_enable(DMA1_Channel1);
+	// -----------------------------------------------------------------
+	// I2C1 Configuration 
+	// -----------------------------------------------------------------
+	I2C1_Load_Default(&I2C_SSD1306_Configuration);
+	I2C_Config(&I2C_SSD1306_Configuration);
+	I2C_IRQ_enable(I2C1, I2Cx_IRQ_BUFFER_DISABLE, I2Cx_IRQ_ERROR_DISABLE);
+	I2C_enable(I2C1);
+
+	// -----------------------------------------------------------------
+	// DMA Configuration
+	// -----------------------------------------------------------------
+	DMA_Load_Default_PER2MEM(&DMA_SSD1306_Configuration);
+	DMA_SSD1306_Configuration.DMA_Channel = DMA_I2C1_TX;
+	DMA_SSD1306_Configuration.data.srcInc = DMAx_INC_ENABLE;
+	DMA_Config(&DMA_SSD1306_Configuration);
+	src_buffer[0] = 0x00;
+	memcpy(src_buffer + 1, SSD1306_initCmd, sizeof(SSD1306_initCmd)/sizeof(SSD1306_initCmd[0]));
+	DMA_Transfer(DMA_I2C1_TX, src_buffer + 1, &I2C1->DR.REG, (sizeof(SSD1306_initCmd)/sizeof(SSD1306_initCmd[0])));
+
+	// -----------------------------------------------------------------
+	// I2C Setup
+	// -----------------------------------------------------------------
+	// Wait for Bus to be Ready
+	while(!I2C_busReady(I2C1));
+	// Send Start Condition
+	I2C_sendStart(I2C1);
+
+	// Wait for Bus to be Ready
+	while(!I2C_busReady(I2C1));
+
+	// -----------------------------------------------------------------
+	// I2C Data Transmission
+	// -----------------------------------------------------------------
+	src_buffer[0] = 0x40;
+	memset(src_buffer + 1, 0xFF, BUFFER_SIZE);
+	DMA_Transfer(DMA_I2C1_TX, src_buffer + 1, &I2C1->DR.REG, BUFFER_SIZE);
+	
+	// Send Start Condition
+	I2C_sendStart(I2C1);
 
 	// Infinite Loop
 	while(1){
 		// Toggle GPIO
 		GPIO_Toggle(&LED_Configuration);
-c		delay_ms(LOOP_DELAY_MS);
+		// Delay
+		delay_ms(LOOP_DELAY_MS);
 	}
 	// Return Value
 	return 0;
 }
+
+
+// void I2C1_EV_IRQHandler(void){
+// 	// 
+// 	switch(i2c_status){
+
+// 		case 0x00:
+// 			if(I2C1->SR1.REG & I2C_SR1_SB){
+// 				I2C1->DR.REG = ((0x3C << 1) | I2Cx_WRITE);
+// 				i2c_status = 0x01;
+// 			}
+// 			break;
+		
+// 		case 0x01:
+// 			if(I2C1->SR1.REG & I2C_SR1_ADDR){
+// 				uint32_t temp = I2C1->SR2.REG;
+// 				I2C1->CR2.REG |= I2C_CR2_DMAEN;
+// 				DMA_I2C1_TX->CCR.REG |= DMA_CCR_EN;
+// 				I2C1->DR.REG = src_buffer[0];
+// 				i2c_status = 0x02;
+// 			}
+// 		break;
+// 	}
+// }
+
+// void DMA1_Channel6_IRQHandler(void){
+// 	// DMA Transfer Complete
+// 	if(DMA1->ISR.REG & DMA_ISR_TCIF6){
+// 		I2C1->CR2.REG &= ~I2C_CR2_DMAEN;
+// 		I2C1->CR1.REG |= I2C_CR1_STOP;
+// 		DMA_I2C1_TX->CCR.REG &= ~DMA_CCR_EN;
+// 		OB_LED_Toggle();
+// 		DMA1->IFCR.REG |= DMA_IFCR_CTCIF6;
+// 		i2c_status = 0x00;
+// 	}
+// }
+
 /*-------------------------------------------------------------------------------*/
-// DMA IRQ Handler
-void DMA1_Channel1_IRQHandler(void){
-	// DMA Transfer Complete
-	if(DMA1->ISR.REG & DMA_ISR_TCIF1){
-		// Toggle OB LED
-		OB_LED_Toggle();
-		// Clear the DMA Transfer Complete Interrupt Flag
-		DMA1->IFCR.REG |= DMA_IFCR_CTCIF1;
+// I2C IRQ Handler
+// /*
+
+void I2C1_EV_IRQHandler(void){
+	// State Machine
+	switch(i2c_status){
+
+		// Initial Status
+		case 0x00:
+			// Start Bit Sent
+			if(I2C1->SR1.REG & I2C_SR1_SB){
+				// Clear SB by sending Address for Write
+				I2C_writeAddress(I2C1, 0x3C);
+				// Update State Machine
+				i2c_status = 0x01;
+			}
+			// Error Condition
+			else
+				i2c_status = 0xFF;
+		break;
+
+		// Address Match
+		case 0x01:
+			// Address Match
+			if(I2C1->SR1.REG & I2C_SR1_ADDR){
+				// Clear ADDR
+				uint32_t temp_reg = I2C1->SR2.REG;
+				// Enable I2C DMA Transfer
+				I2C_DMA_enable(I2C1);
+				// Update State Machine
+				i2c_status = 0x02;
+			}
+			// Error Condition
+			else
+				i2c_status = 0xFE;
+		break;
+
+		// STOP
+		case 0x03:
+			// Byte Transfer Completed
+			if(I2C1->SR1.REG & I2C_SR1_BTF){
+				// 
+				I2C_sendStop(I2C1);
+				// Indication
+				OB_LED_Toggle();
+				// Bus Free
+				if(I2C_busReady(I2C1)){
+					// Reset State Machine
+					i2c_status = 0x00;
+				}
+			}
+		break;
+		// Unaccounted States
+		default:
+		break;
 	}
 }
-
+// */
+/*-------------------------------------------------------------------------------*/
+// DMA IRQ Handler
+// /*
+void DMA1_Channel6_IRQHandler(void){
+	// DMA Transfer Complete
+	if(DMA1->ISR.REG & DMA_ISR_TCIF6){
+		// Disable the Channel
+		DMA_CH_disable(DMA_I2C1_TX);
+		// Disable I2C DMA
+		I2C_DMA_disable(I2C1);
+		// Sent Stop Condition
+		I2C_sendStop(I2C1);
+		// Update State Machine
+		i2c_status = 0x03;
+		// Bus Free
+		if(I2C_busReady(I2C1)){
+			// Indication
+			OB_LED_Toggle();
+			// Reset
+			i2c_status = 0x00;
+		}
+		// Clear the DMA Transfer Complete Interrupt Flag
+		DMA1->IFCR.REG |= DMA_IFCR_CTCIF6;
+	}
+}
+// */
+/*-------------------------------------------------------------------------------*/
+/**
+ * @brief Error Protection
+ */
+void I2C1_ER_IRQHandler(void){
+	// Arbitration Lost Flag
+	if(I2C1->SR1.REG & I2C_SR1_AF) {
+		// Clear NACK flag
+		I2C1->SR1.REG &= ~I2C_SR1_AF;
+		// Force stop
+		I2C1->CR1.REG |= I2C_CR1_STOP;
+		// Reset state machine
+		i2c_status = 0x00;
+	}
+}
 /*-------------------------------------------------------------------------------*/
